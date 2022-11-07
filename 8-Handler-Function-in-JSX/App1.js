0d66a0e72a18764d359f3bb2ea9f3b6502aa76e9
Mark Dondi Aberte <116357985+DKram11@users.noreply.github.com>
2022-11-07 20:43:59 +0800

    Update App1.js

--- 8-Handler-Function-in-JSX/App1.js
+++ 8-Handler-Function-in-JSX/App1.js
@@ -5,7 +5,7 @@ const Search = () => {
          <div> 
             <label htmlFor="search">Search: </label> 
             <input id="search" type="text" /> 
-        </div> 
+         </div> 
     );
  };
     
